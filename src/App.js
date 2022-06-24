2b59d482ad7fcd2d428a92e39599279037ae0dfd
Hiten Vishwakarma <hiten997764@gmail.com>
2022-06-24 18:08:42 +0530

    login page completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./component/Form";
+import { Home } from "./component/Home";
 
-function App() {
+const App = () => {
+  const [isLoggedin, setIsLoggedin] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!isLoggedin && <Form login={setIsLoggedin} />}
+      {isLoggedin && <Home />}
     </div>
   );
-}
+};
 
 export default App;
